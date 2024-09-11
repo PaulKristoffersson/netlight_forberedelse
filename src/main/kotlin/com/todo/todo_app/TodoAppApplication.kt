2fd97e5da5a7ae98f682ad622fbf329d89c4b7f1
paulkristoffersson <paul.blaberg.kristoffersson@svenskdrift.se>
2024-09-11 15:15:04 +0200

    first commit

---
+++ src/main/kotlin/com/todo/todo_app/TodoAppApplication.kt
@@ -0,0 +1,12 @@
+package com.todo.todo_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoAppApplication>(*args)
+    println("hej")
+}
