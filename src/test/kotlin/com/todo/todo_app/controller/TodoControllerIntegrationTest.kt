ce555c230677c80958ed89a6b874063b8c14701a
paulkristoffersson <paul.blaberg.kristoffersson@svenskdrift.se>
2024-09-13 18:39:05 +0200

    first integration test complete

---
+++ src/test/kotlin/com/todo/todo_app/controller/TodoControllerIntegrationTest.kt
@@ -0,0 +1,12 @@
+package com.todo.todo_app.controller
+
+import org.junit.jupiter.api.BeforeEach
+
+import org.junit.jupiter.api.Assertions.*
+
+class TodoControllerIntegrationTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+}
