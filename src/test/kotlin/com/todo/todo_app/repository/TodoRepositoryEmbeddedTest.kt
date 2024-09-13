38f7144c37722223aaa5f52cffeaf388c9c6816e
paulkristoffersson <paul.blaberg.kristoffersson@svenskdrift.se>
2024-09-13 16:14:48 +0200

    added test data

---
+++ src/test/kotlin/com/todo/todo_app/repository/TodoRepositoryEmbeddedTest.kt
@@ -0,0 +1,4 @@
+package com.todo.todo_app.repository
+
+class TodoRepositoryEmbeddedTest {
+}
---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO todo
+(`id`, `createdAt`, `updatedAt`, `title`, `description`, `rank`, 'done')
+VALUES (401cb1e7-7911-4fa0-b740-fac9eb24bbe6, CURRENT_TIME(), CURRENT_TIME(), 'first todo', 'test for todo', 1);
+
+INSERT INTO todo
+(`id`, `createdAt`, `updatedAt`, `title`, `description`, `rank`, 'done')
+VALUES (da8351c5-a947-4fc2-8dc9-eab632ad8707, CURRENT_TIME(), CURRENT_TIME(), 'second todo', 'test for todo', 3);
+
+INSERT INTO todo
+(`id`, `createdAt`, `updatedAt`, `title`, `description`, `rank`, 'done')
+VALUES (801f7917-5650-4f7b-904f-0b3ed17d24f5, CURRENT_TIME(), CURRENT_TIME(), 'third todo', 'test for todo', 2);
