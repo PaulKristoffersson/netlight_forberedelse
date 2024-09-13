057b312d0eceb581b7ca8323d9527109721911a6
paulkristoffersson <paul.blaberg.kristoffersson@svenskdrift.se>
2024-09-13 16:21:00 +0200

    Start creating Jpa Test

--- src/test/kotlin/com/todo/todo_app/repository/TodoRepositoryEmbeddedTest.kt
+++ src/test/kotlin/com/todo/todo_app/repository/TodoRepositoryEmbeddedTest.kt
@@ -1,4 +1,16 @@
 package com.todo.todo_app.repository
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest(properties =  ["spring.jpa.properties.javax.persistence.validation.mode=none"])
 class TodoRepositoryEmbeddedTest {
+
+    @Autowired
+    private lateinit var objectUnderTest: TodoRepository
+
+    private val numberOfRecordsInTestData = 3
+    private val numberOfCompletedRecordsInTestData = 2
+    private val numberOfUncompletedRecordsInTestData = 1
+
 }
